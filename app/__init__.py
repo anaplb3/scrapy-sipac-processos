d9a50a34e016873c3a43890e4c794744f64c6510
anaplb3 <ana.paula@dcx.ufpb.br>
2020-07-09 23:29:08 -0300

    retirando endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
 from app.main.controller.processos_controller import api as processo_namespace
 from app.main.controller.keepalive_controller import api as keepalive_namespace
-from app.main.controller.populando_banco_controller import api as populando_namespace
 from flask import Flask, Blueprint, url_for
 from flask_restx import Api, Resource, apidoc
 
@@ -30,4 +29,3 @@ api = MyCustomApi(blueprint, version='1.0', doc="/docs")
 
 api.add_namespace(processo_namespace, path="/processos")
 api.add_namespace(keepalive_namespace, path="/keepalive")
-api.add_namespace(populando_namespace, path="/populandobanco")
