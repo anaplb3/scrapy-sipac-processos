84e997ffb34735bc499919ab7d400bcda5e17f6e
anaplb3 <ana.paula@dcx.ufpb.br>
2020-08-11 16:51:35 -0300

    tentando consertar erro do chromium

--- app/main/web_scrapy/sipac_selenium.py
+++ app/main/web_scrapy/sipac_selenium.py
@@ -31,7 +31,7 @@ def setting_selenium():
         return webdriver.Chrome("C:\chromedriver")
     else:
         chrome_options = webdriver.ChromeOptions()
-        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        """ chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN") """
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--no-sandbox")
