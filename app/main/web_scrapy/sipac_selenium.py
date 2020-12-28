8352b10d6499231bcbd1c506965aaefd85a7c0f6
anaplb3 <ana.paula@dcx.ufpb.br>
2020-12-28 17:05:07 -0300

    tentando consertar erro do chrome misterioso que sempre aparece

--- app/main/web_scrapy/sipac_selenium.py
+++ app/main/web_scrapy/sipac_selenium.py
@@ -31,10 +31,11 @@ def setting_selenium():
         return webdriver.Chrome("C:\chromedriver")
     else:
         chrome_options = webdriver.ChromeOptions()
-        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_argument("--no-sandbox")
+        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        
         driver = webdriver.Chrome(executable_path=os.environ.get(
             "CHROMEDRIVER_PATH"), chrome_options=chrome_options)
         return driver
